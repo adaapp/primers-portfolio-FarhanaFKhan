b35a17d896c21111500a9e8b1ac2e4a7cce97c27
Ffkhan <61620091+FarhanaFKhan@users.noreply.github.com>
2021-08-17 16:05:33 +0000

    Primer 4- needs refactoring

--- include/tuesday.h
+++ include/tuesday.h
@@ -3,21 +3,46 @@ void passwordComplexityChecker(void) {
 }
 
 
+
+//helper function to find whether the name exists in the employeeListRemoval
+// int getIndex(std::vector<std::string> emp, std::string name){
+//   int it = std::find(emp.cbegin(), emp.cend(), name);
+//   std::cout << it;
+//   //if the name is found
+//   if(it != emp.cend()){
+//     int index = std::distance(emp.cbegin(),it);
+//     return index;
+//   }else{
+//     //if not found
+//     return -1;
+//   }
+// }
+
 void employeeListRemoval(void) {
 	
   //store names of employess in a vector as it is easier to manipulate than an array in C++
   std::string employeeToRemove;
+  int indexOfEmp;
 
   std::vector<std::string> employees = {"John Smith", "Jaelynn Stuart", "Kaley Barajas","Walter Collier", "Cale Myers"};
+  
+  std::cout << "There are " << employees.size() << " employess:\n";
+  for(int i; i <employees.size(); i++){
+    std::cout << employees[i] << "\n";
+  }  
+
+  std::cout << "Enter an employee name to remove: ";
+  std::getline(std::cin,employeeToRemove);
+
+  for(int i = 0; i < employees.size(); i++){
+    if(employees[i] == employeeToRemove){
+      employees.erase(employees.cbegin()+i);
+    }
+  } 
+
   std::cout << "There are " << employees.size() << " employess:\n";
   for(int i; i <employees.size(); i++){
     std::cout << employees[i] << "\n";
   }
-  
-
-  std::cout << "Enter an employee name to remove: ";
-  std::cin >> employeeToRemove;
-
-
 
 }
