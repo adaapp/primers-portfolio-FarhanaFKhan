a644d3c02f8c5b6687cd47a605711858f6ba5ff2
Ffkhan <61620091+FarhanaFKhan@users.noreply.github.com>
2021-08-18 13:02:34 +0000

    Primer 5- Get number of records from the file

--- include/tuesday.h
+++ include/tuesday.h
@@ -5,7 +5,8 @@ void passwordComplexityChecker(void) {
 
 
 //helper function to find whether the name exists in the employee list
-int getIndex(std::vector<std::string> emp, std::string name){
+//pass pointer rather than a copy
+int findEmployee(std::vector<std::string> emp, std::string name){
   auto it = std::find(emp.cbegin(), emp.cend(), name);
   //if the name is found
   if(it != emp.cend()){
@@ -21,29 +22,29 @@ void employeeListRemoval(void) {
 	
   //store names of employess in a vector as it is easier to manipulate than an array in C++
   std::string employeeToRemove;
-  int indexOfEmp;
+  int isEmployee;
 
   std::vector<std::string> employees = {"John Smith", "Jaelynn Stuart", "Kaley Barajas","Walter Collier", "Cale Myers"};
 
-  std::cout << "There are " << employees.size() << " employess:\n";
-  for(int i; i <employees.size(); i++){
+  std::cout << "\nThere are " << employees.size() << " employees:\n";
+  for(int i; i < employees.size(); i++){
     std::cout << employees[i] << "\n";
   }  
 
-  std::cout << "Enter an employee name to remove: ";
+  std::cout << "\nEnter an employee name to remove: ";
   std::getline(std::cin,employeeToRemove);
 
-  indexOfEmp = getIndex(employees, employeeToRemove);
+  isEmployee = findEmployee(employees, employeeToRemove);
 
-  if(indexOfEmp < 0){
-    std::cout << "Record not found! \n";
+  if(isEmployee < 0){
+    std::cout << "\nRecord not found! \n";
   }else{
     for(int i = 0; i < employees.size(); i++){
       if(employees[i] == employeeToRemove){
       employees.erase(employees.cbegin()+i);
       }
     }
-    std::cout << "There are " << employees.size() << " employess:\n";
+    std::cout << "\nThere are " << employees.size() << " employess:\n";
     for(int i; i <employees.size(); i++){
     std::cout << employees[i] << "\n";
     }
--- include/wednesday.h
+++ include/wednesday.h
@@ -1,5 +1,25 @@
+//read function to read data from the file phonedirectory.csv
+void read(std::string file){
+  std::string line;
+  std::ifstream fileObject;
+  int lineNumber;
+
+  fileObject.open(file);
+  while(!fileObject.eof()){
+    getline(fileObject,line);
+    lineNumber++;
+  }
+  std::cout << "\nSearching " << lineNumber <<" records..." << std::endl;
+  fileObject.close();
+}
+
 void phoneDirectory(void) {
-	std::cout << " - phoneDirectory: not yet implemented\n\n";
+  std::string searchWord;
+	std::cout << "Please enter a name or number to search:";
+  std::getline(std::cin,searchWord);
+  std::cout << "Search word: " << searchWord;
+  read("phonedirectory.csv");
+
 }
 
 
