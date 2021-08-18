f13f7f92cf4a750ad7d8da7896d93b5e6459fff6
Ffkhan <61620091+FarhanaFKhan@users.noreply.github.com>
2021-08-18 13:46:39 +0000

    Find record by searching full name

--- include/wednesday.h
+++ include/wednesday.h
@@ -1,3 +1,8 @@
+//helper function to search
+
+
+
+
 //read function to read data from the file phonedirectory.csv
 void read(std::string file, std::string& searchTerm){
   std::string line;
@@ -6,28 +11,37 @@ void read(std::string file, std::string& searchTerm){
   std::string phoneNumber;
   int lineNumber;
   bool found = false;
+  std::string recordFound;
 
   fileObject.open(file);
   while(!fileObject.eof()){
     getline(fileObject,line);
+    lineNumber++;
     std::stringstream iss(line);
     std::getline(iss, name, ',');
-
-    std::cout <<"\nJust name: " << name;
-    
-    lineNumber++;
+    if(name == searchTerm){
+      //std::cout <<"\n" << line << std::endl;
+      found = true;
+      recordFound = line;
+    }    
   }
+
   std::cout << "\nSearching " << lineNumber <<" records..." << std::endl;
+  if(found){
+    std::cout << "\nContact details:\n" << recordFound;
+  }else{
+    std::cout << "Sorry, no contact details found"<< std::endl;
+  }
   fileObject.close();
 }
 
 void phoneDirectory(void) {
   std::string searchWord;
+
 	std::cout << "Please enter a name or number to search:";
   std::getline(std::cin,searchWord);
-  std::cout << "Search word: " << searchWord;
+  
   read("phonedirectory.csv", searchWord);
-
 }
 
 
