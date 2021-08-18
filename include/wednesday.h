4440963234af99fb17d2862e401f61b2d01ec335
Ffkhan <61620091+FarhanaFKhan@users.noreply.github.com>
2021-08-18 13:31:09 +0000

    parse name from the csv file

--- include/wednesday.h
+++ include/wednesday.h
@@ -1,12 +1,20 @@
 //read function to read data from the file phonedirectory.csv
-void read(std::string file){
+void read(std::string file, std::string& searchTerm){
   std::string line;
   std::ifstream fileObject;
+  std::string name;
+  std::string phoneNumber;
   int lineNumber;
+  bool found = false;
 
   fileObject.open(file);
   while(!fileObject.eof()){
     getline(fileObject,line);
+    std::stringstream iss(line);
+    std::getline(iss, name, ',');
+
+    std::cout <<"\nJust name: " << name;
+    
     lineNumber++;
   }
   std::cout << "\nSearching " << lineNumber <<" records..." << std::endl;
@@ -18,7 +26,7 @@ void phoneDirectory(void) {
 	std::cout << "Please enter a name or number to search:";
   std::getline(std::cin,searchWord);
   std::cout << "Search word: " << searchWord;
-  read("phonedirectory.csv");
+  read("phonedirectory.csv", searchWord);
 
 }
 
