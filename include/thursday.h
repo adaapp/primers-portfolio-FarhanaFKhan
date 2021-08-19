da5b18d264eccf288c9d1aa078feff1ab81893e8
Ffkhan <61620091+FarhanaFKhan@users.noreply.github.com>
2021-08-19 16:18:21 +0000

    Primer 8 - create multithreads

--- include/thursday.h
+++ include/thursday.h
@@ -8,7 +8,28 @@ void sleepTimer(void) {
 	sleepnow();
 }
 
+//creating a thread
+void thread_function(){
+  
+  std::cout << std::this_thread::get_id() <<" started\n";
+  sleepnow();
+  std::cout << std::this_thread::get_id() <<" ended\n";
+  
+}
+
 //primer 8
 void joinDetachThreads(void) {
-	std::cout << " - joinDetachThreads: not yet implemented\n\n";
+	std::cout <<"Main thread: "<< std::this_thread::get_id() << " started\n";
+  
+  sleepnow(5);
+  std::cout << "Thread 1: ";
+  std::thread t1(&thread_function);
+  sleepnow(5);
+  std::cout << "Thread 2: ";
+  std::thread t2(&thread_function);
+
+  t1.join();
+  t2.join();
+
+  std::cout <<"Main thread: "<< std::this_thread::get_id() << " ended\n";
 }
