a65871a3b2f62e24da43a2c800942d06fb34b4da
Ffkhan <61620091+FarhanaFKhan@users.noreply.github.com>
2021-08-19 16:51:06 +0000

    Primer 8- complete

--- include/thursday.h
+++ include/thursday.h
@@ -9,24 +9,33 @@ void sleepTimer(void) {
 }
 
 //creating a thread
-void thread_function(){
+void thread1(){
   
-  std::cout << std::this_thread::get_id() <<" started\n";
+  std::cout << "Thread 1: "<< std::this_thread::get_id() <<" started\n";
+  sleepnow(5);
+  std::cout << "Thread 1: "<< std::this_thread::get_id() <<" ended\n";
+  
+}
+
+void thread2(){
+  
+  std::cout << "Thread 2: "<< std::this_thread::get_id() <<" started\n";
   sleepnow();
-  std::cout << std::this_thread::get_id() <<" ended\n";
+  std::cout << "Thread 2: "<< std::this_thread::get_id() <<" ended\n";
   
 }
 
 //primer 8
+//thread1 is initialised after 5 seconds by calling the sleep now function,
+//then thread2 is initialised. join() pauses the main thread and waits till thread1 and thread 2 complete execution.
 void joinDetachThreads(void) {
-	std::cout <<"Main thread: "<< std::this_thread::get_id() << " started\n";
   
+	std::cout <<"Main thread: "<< std::this_thread::get_id() << " started\n";
+
   sleepnow(5);
-  std::cout << "Thread 1: ";
-  std::thread t1(&thread_function);
-  sleepnow(5);
-  std::cout << "Thread 2: ";
-  std::thread t2(&thread_function);
+
+  std::thread t1(&thread1);
+  std::thread t2(&thread2);  
 
   t1.join();
   t2.join();
