eb7ab185cb29f0e1df3f073501ebd0d2f0d01a93
Ffkhan <61620091+FarhanaFKhan@users.noreply.github.com>
2021-08-20 14:31:23 +0000

    set engine off as default

--- include/friday.h
+++ include/friday.h
@@ -17,29 +17,50 @@ class Car {
     std::cout <<"\nCreated";
     set_colour();
     set_make();
+    engine_off();
   }
   ~Car() {
     std::cout <<"\nDestroyed";
   }
 
-  void set_colour(std::string colour = "Black"){
+  void set_colour(std::string colour = "Black") {
     car_colour = colour;
   }
 
-  std::string get_colour(){
+  std::string get_colour(void) {
     return car_colour;
   }
 
-  void set_make(std::string make = "Audi"){
+  void set_make(std::string make = "Audi") {
     car_make = make;
   }
 
-  std::string get_make(){
+  std::string get_make(void) {
     return car_make;
   }
 
+  void engine_on(void) {
+    if(is_engine_on) {
+      std::cout << "\nSorry the engine is already on";
+    } else {
+      is_engine_on = true;
+    }    
+  }
+
+  void engine_off(void) {
+    if(is_engine_on) {
+      is_engine_on = false;
+    } else {
+      std::cout << "\nSorry the engine is already off";
+    }
+  }
+
   void status(void) {
-    std::cout <<"\nCar Status: color: " << car_colour << " , make: " << car_make;
+    std::string engine_status = "Off";
+    if(is_engine_on){
+      engine_status = "On";
+    }
+    std::cout <<"\nCar Status: color: " << car_colour << " , make: " << car_make << " ,engine : " << engine_status;
   }
   
 };
