e9f8dc0fe8065636e1aef7137fb4121d29c27297
Ffkhan <61620091+FarhanaFKhan@users.noreply.github.com>
2021-08-20 14:19:06 +0000

    Set default car colour and make in ctor.

--- include/friday.h
+++ include/friday.h
@@ -5,8 +5,9 @@
 
 class Car {
   private:
-  std::string car_colour;
   const std::string DEFAULT_COLOUR = "Black";
+
+  std::string car_colour;
   std::string car_make;
   bool is_engine_on = false;
   bool is_locked = false;
@@ -14,17 +15,39 @@ class Car {
   public:
   Car() {
     std::cout <<"\nCreated";
+    set_colour();
+    set_make();
   }
   ~Car() {
     std::cout <<"\nDestroyed";
   }
 
+  void set_colour(std::string colour = "Black"){
+    car_colour = colour;
+  }
+
+  std::string get_colour(){
+    return car_colour;
+  }
+
+  void set_make(std::string make = "Audi"){
+    car_make = make;
+  }
+
+  std::string get_make(){
+    return car_make;
+  }
+
+  void status(void) {
+    std::cout <<"\nCar Status: color: " << car_colour << " , make: " << car_make;
+  }
   
 };
 
 
 void carClass(void) {
 		Car new_car;
+    new_car.status();
 }
 
 
